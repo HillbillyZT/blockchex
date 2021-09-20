fdebe67ed469db95b9a42a553f45525c45b728cf
chris30257 <chris30257@gmail.com>
2021-09-20 01:34:48 -0500

    Begin p2p.py skeleton

--- p2p.py
+++ p2p.py
@@ -1,4 +1,23 @@
 import websockets
 import asyncio
+import json
+from enum import Enum
+
+class MessageType(Enum):
+    QUERY_LATEST = 0
+    QUERY_ALL = 1
+    RESPONSE_BLOCKCHAIN = 2
+
+class Message:
+    def __init__(self, type: MessageType, data: str):
+        self.type = type
+        self.data = data
+
+# We have a server waiting for incoming connection requests
+def init_P2P(websocket) -> None:
+    pass
+    
+
+
 
 
